287e8b54f440081671b69bb9d875a7eed03998b2
ABC <80405362+q1016390482@users.noreply.github.com>
2026-08-08 17:06:10 +0800

    Add OpenClash package to diy.sh script

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -17,6 +17,8 @@ git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-t
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+# OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash package/luci-app-openclash
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 
 git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
